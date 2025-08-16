efe9a9311da8a8fd7be62470ffe2a953ef73bdef
batman112001 <namtab112001@gmail.com>
2025-08-16 21:45:52 +0530

    Update file.js

--- functions/api/file.js
+++ functions/api/file.js
@@ -1,9 +1,9 @@
 export async function onRequestGet({ request, env }) {
-  const { FILES } = env;
+  const { FILES_M4 } = env;   // changed
   const id = new URL(request.url).searchParams.get("id");
   if (!id) return Response.json({ error: "Missing id" }, { status: 400 });
 
-  const url = await FILES.get(id);
+  const url = await FILES_M4.get(id);
   if (!url) return Response.json({ error: "Not found" }, { status: 404 });
 
   return Response.json({ id, url });
